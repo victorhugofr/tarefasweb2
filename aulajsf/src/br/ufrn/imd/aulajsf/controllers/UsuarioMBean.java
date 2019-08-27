990c350e4493847ece5098659485433896443be6
Victor Hugo Freire Ramalho <vale1c@A307-08L.imd.local>
2019-08-27 14:21:16 -0300

    atualizacaoprojeto

---
+++ aulajsf/src/br/ufrn/imd/aulajsf/controllers/UsuarioMBean.java
@@ -0,0 +1,52 @@
+package br.ufrn.imd.aulajsf.controllers;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+import br.ufrn.imd.aulajsf.dominio.Usuario;
+import br.ufrn.imd.aulajsf.repositorios.UsuarioRepositorio;
+
+@Named("usuarioMBean")
+@SessionScoped
+public class UsuarioMBean implements Serializable {
+	private Usuario usuario;
+	private Usuario usuarioLogado;
+
+	public String logar() {
+		Usuario usuarioBd = UsuarioRepositorio.getUsuario(usuario.getLogin(), usuario.getSenha());
+		if (usuarioBd != null) {
+			usuarioLogado = usuarioBd;
+			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuarioLogado", usuarioLogado);
+			return "/pages/index.jsf";
+		} else {
+			FacesMessage msg = new FacesMessage("Usuario não encontrado");
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			FacesContext.getCurrentInstance().addMessage("", msg);
+			return null;
+		}
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Usuario getUsuarioLogado() {
+		return usuarioLogado;
+	}
+
+	public void setUsuarioLogado(Usuario usuarioLogado) {
+		this.usuarioLogado = usuarioLogado;
+	}
+
+	public UsuarioMBean() {
+		usuario = new Usuario();
+	}
+}
--- aulajsf/src/br/ufrn/imd/aulajsf/dominio/Usuario.java
+++ aulajsf/src/br/ufrn/imd/aulajsf/dominio/Usuario.java
@@ -4,30 +4,39 @@ public class Usuario {
 	private String login;
 	private String senha;
 	private String nome;
-	
-	public Usuario(String login, String senha) {
-		this.login=login;
-		this.senha=senha;
+
+	public Usuario() {
 	}
-	
+
+	public Usuario(String login, String senha) {
+		this.login = login;
+		this.senha = senha;
+	}
+
 	public String getLogin() {
 		return login;
 	}
+
 	public void setLogin(String login) {
 		this.login = login;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -35,6 +44,7 @@ public class Usuario {
 		result = prime * result + ((login == null) ? 0 : login.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
